05130ca510383c29da6c4272d64d5d4e0e05ff45
himyon <himyon@gmail.com>
2017-08-13 01:12:44 +0900

    Add 04-if-statement.py

---
+++ 05-for-loop.py
@@ -0,0 +1,19 @@
+# for, range (for는 ~하는 동안)
+# while
+
+# for (range는 범위)
+for num in range(1, 10):
+    print(num)
+
+num_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for num in num_list:
+    print(num)
+
+# while (if와 for 가 결합된 거라 생각하면 됨)
+# while True: #무한루프
+#     print(1)
+
+a = 1
+while a < 10:
+    print(a)
+    a = a + 1
