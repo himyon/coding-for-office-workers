2b541490e96c19d0eac1c6f259e21c37a88bc553
himyon <himyon@gmail.com>
2017-08-16 23:55:42 +0900

    Add 07-functions.py

---
+++ 07-functions.py
@@ -0,0 +1,34 @@
+# # 함수
+# # 입력값 parameters, 반환값 return
+
+def hello_friends(name):
+    print("hello, {}".format(name))
+
+name1 = "hhhh"
+name2 = "john"
+name3 = "kkklp"
+name4 = "mattew"
+name5 = "hhhh"
+name6 = "john"
+name7 = "kkklp"
+name8 = "mattew"
+
+hello_friends(name1)
+hello_friends(name2)
+hello_friends(name3)
+hello_friends(name4)
+hello_friends(name5)
+hello_friends(name6)
+hello_friends(name7)
+hello_friends(name8)
+
+
+
+# print("hi, {}".format(name1))
+# print("hi, {}".format(name2))
+# print("hi, {}".format(name3))
+# print("hi, {}".format(name4))
+# print("hi, {}".format(name5))
+# print("hi, {}".format(name6))
+# print("hi, {}".format(name7))
+# print("hi, {}".format(name8))
---
+++ mini-assignment.py
@@ -0,0 +1,5 @@
+
+# 미니과제
+import random
+a = [1,2,3,4,5]
+print(random.choice(a))
