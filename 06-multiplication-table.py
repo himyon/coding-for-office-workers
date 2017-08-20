f3a487bc297f65592d44e704258da20d60199122
himyon <himyon@gmail.com>
2017-08-21 00:02:36 +0900

    Add 06-multiplication-table.py

--- 06-multiplication-table.py
+++ 06-multiplication-table.py
@@ -8,6 +8,12 @@
 #     print(int(name), "*", b, "=", int(name) * b)
 #     b = b + 1
 
+# format type use
 name = int(input("몇 단을 출력하시겠습니까?"))
 for num in range(1, 10):
     print("{} * {} = {}".format(name, num, name * num))
+
+# for문 사용
+name = int(input("몇 단을 출력하시겠습니까?"))
+for num in range(1, 10):
+    print(name, "*", num, "=", name * num)
