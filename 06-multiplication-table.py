afa1449dc1ef9b0d644b5a12c69fe539dcef82d4
himyon <himyon@gmail.com>
2017-08-20 23:55:05 +0900

    Add 06-multiplication-table.py

--- 06-multiplication-table.py
+++ 06-multiplication-table.py
@@ -1,9 +1,13 @@
+#
+# name = input("몇 단을 출력하시겠습니까? ")
+#
+# # input(" ")
+# # a = input(" ")
+# b = 1
+# while b < 10:
+#     print(int(name), "*", b, "=", int(name) * b)
+#     b = b + 1
 
-name = input("몇 단을 출력하시겠습니까? ")
-
-# input(" ")
-# a = input(" ")
-b = 1
-while b < 10:
-    print(int(name), "*", b, "=", int(name) * b)
-    b = b + 1
+name = int(input("몇 단을 출력하시겠습니까?"))
+for num in range(1, 10):
+    print("{} * {} = {}".format(name, num, name * num))
