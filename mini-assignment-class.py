efbb15d48bf1e221794870e6213cafdb8c6012fe
himyon <himyon@gmail.com>
2017-08-24 23:43:10 +0900

    Add mini-assignment-class.py

---
+++ mini-assignment-class.py
@@ -0,0 +1,13 @@
+# 학교 클래스
+
+class School:
+    name = "성보중학교"
+    year = "1999년"
+    adress = "서울시_관악구_신림동"
+
+    def __init__(self, name, year, adress):
+        self.name = name
+        self.year = year
+        self.adress = adress
+
+School1 = School("성보중학교", "1999년", "서울시_관악구_신림동")
