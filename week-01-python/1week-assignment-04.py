0193bec6e3dde47024a43ba4edf83a2ffd0fcd9d
himyon <himyon@gmail.com>
2017-09-06 23:05:51 +0900

    Change directory

---
+++ week-01-python/1week-assignment-04.py
@@ -0,0 +1,20 @@
+# for num in range(0, 2):
+#     print(num)
+
+a = "O"
+b = 1
+c = 0
+print(a, a, b, a, a)
+print(a, b, b, b, a)
+print(a, a, a, a, a)
+print(a, b, b, b, a)
+print(a, a, b, a, a)
+
+
+# while True:
+    # print(a , b)
+    # print(a, a, b, a, a, a, b, b, b, a, a, a, a, a, a, a, b, b, b, a, a, a, b, a, a)
+    # print(a, b, b, b, a)
+    # print(a, a, a, a, a)
+    # print(a, b, b, b, a)
+    # print(a, a, b, a, a)
